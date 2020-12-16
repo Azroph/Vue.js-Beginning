4a8262131bf6dd11c0af68411ab01611682ae2ac
Azroph <56892575+Azroph@users.noreply.github.com>
2020-12-16 21:32:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data(){
+        return{
+
+            name: "Bruce Lee",
+            year: 32,
+            old: "years old"
+
+        }
+    }
+});
+
+app.mount('#app')
